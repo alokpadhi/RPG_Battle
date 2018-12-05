477b937542d8999dbdc4294ef1b4cf06232b084b
alokpadhi <alokkumarpadhi198@gmail.com>
2018-12-05 23:34:01 +0530

    game settings

---
+++ classes/game.py
@@ -0,0 +1,86 @@
+import random
+
+
+class Bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+class Person:
+
+    def __init__(self, hp, mp, atck, df, magic):
+        """
+        :param hp: hit points
+        :param mp:
+        :param atck: attack
+        :param df: defense
+        :param magic: magic
+        """
+        # setting up instance variables
+        # maxhp will hold the maximum hit point value
+        self.maxhp = hp
+        # as hp will change throughout the game, we will update that
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atckh = atck + 10
+        self.atckl = atck - 10
+        self.df = df
+        self.magic = magic
+        self.actions = ["Attack", "Magic"]
+
+    def generate_damage(self):
+        return random.randrange(self.atckl, self.atckh)
+
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        print(Bcolors.OKBLUE + Bcolors.BOLD + "Actions" + Bcolors.ENDC)
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print(Bcolors.OKBLUE + Bcolors.BOLD + "Magic" + Bcolors.ENDC)
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
+            i += 1
+
