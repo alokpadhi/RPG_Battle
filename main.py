d47973662464bb490115362f0f1f7d07e0c04f82
alokpadhi <alokkumarpadhi198@gmail.com>
2018-12-07 00:59:43 +0530

    driver program updated with magic.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from classes.game import Person, Bcolors
+from classes.magic import Spell
 
+# Creating black magic actions
+fire = Spell("Fire", 10, 100, "Black")
+thunder = Spell("Thunder", 10, 100, "Black")
+blizzard = Spell("Blizzard", 10, 100, "Black")
+meteor = Spell("Meteor", 20, 200, "Black")
+quake = Spell("Quake", 12, 140, "Black")
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 100},
-         {"name": "Thunder", "cost": 10, "dmg": 124},
-         {"name": "Blizzard", "cost": 10, "dmg": 100}]
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+# creating white magic actions
+cure = Spell("Cure", 12, 120, "White")
+cura = Spell("Cura", 18, 200, "White")
+
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, quake, cure, cura])
+enemy = Person(1200, 65, 45, 25, [fire, thunder, blizzard, meteor, quake])
 
 running = True
 i = 0
@@ -25,17 +33,18 @@ while running:
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input("Choose magic: ")) - 1
-        magic_dmg = player.generate_spell_damage(magic_choice)
-        spell = player.get_spell_name(magic_choice)
-        cost = player.get_spell_mp_cost(magic_choice)
+
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
 
         current_mp = player.get_mp()
-        if cost > current_mp:
+
+        if spell.cost > current_mp:
             print(Bcolors.FAIL + "\nNot Enough MP\n" + Bcolors.ENDC)
             continue
-        player.reduce_mp(cost)
+        player.reduce_mp(spell.cost)
         enemy.take_damage(magic_dmg)
-        print(Bcolors.OKBLUE + "\n" + spell + " deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
+        print(Bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
 
     enemy_choice = 1
     enemy_dmg = enemy.generate_damage()
